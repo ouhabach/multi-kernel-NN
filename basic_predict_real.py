afc40e303ca5c54750f4720ffdf8663ccf9da3c0
ouhabach <oussama.habachi@gmail.com>
2025-07-23 09:42:02 +0100

    Update basic_predict_real.py

--- basic_predict_real.py
+++ basic_predict_real.py
@@ -6,20 +6,25 @@ real_values = y_test
 predicted_values = y_pred
 
 # Calcul des pertes
-loss = np.abs(real_values - predicted_values) / np.maximum(real_values, predicted_values)
+loss = np.abs(np.array(real_values) - np.array(predicted_values)) / np.maximum(np.array(real_values), np.array(predicted_values))
 loss_abs = np.abs(real_values - predicted_values)
 
 # Affichage des pertes moyennes
 print(f"loss: {np.mean(loss)}")
+print(np.array(real_values) - np.array(predicted_values))
+print(np.argmax(loss))
+print(real_values[18243]) 
+print(predicted_values[18243]) 
+print(f"loss: {np.max(loss)}")
 print(f"loss abs: {np.mean(loss_abs)}")
 
 # Tracer la courbe de régression
 plt.figure(figsize=(8, 6))
-plt.scatter(real_values, predicted_values, alpha=0.7, color='blue', label='Prédictions')
-plt.plot([min(real_values), max(real_values)], [min(real_values), max(real_values)], color='red', linestyle='--', label='Régression idéale')
-plt.title("Réel vs Prédit - Sum Rate Maximal")
-plt.xlabel("Valeurs réelles")
-plt.ylabel("Valeurs prédites")
+plt.scatter(real_values, predicted_values, alpha=0.7, color='blue', label='Predicted')
+plt.plot([min(real_values), max(real_values)], [min(real_values), max(real_values)], color='red', linestyle='--', label='Ideal regression')
+#plt.title("Réel vs Prédit - Sum Rate Maximal")
+plt.xlabel("Real Sum-rate")
+plt.ylabel("Predicted Sum-rate")
 plt.legend()
 plt.grid(True)
 
@@ -27,4 +32,3 @@ plt.grid(True)
 plt.savefig("reel_vs_predit.png", dpi=300, bbox_inches='tight')
 
 plt.show()
-
